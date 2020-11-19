0d6a3d969e21ec46a91f44b3a4634c136c58e882
David Brown <dmlb2000@gmail.com>
2020-11-18 20:58:58 -0800

    Add Script Output Logging
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -xe
 
-/opt/run-fastq-multx.sh
-
-printf 'do_processing,False' > output_fastq_multx/kvp.csv
-
-/opt/run-hundo.sh
-
-printf 'do_processing,False' > output_hundo/kvp.csv
+(
+    /opt/run-fastq-multx.sh
+    printf 'do_processing,False' > output_fastq_multx/kvp.csv
+    /opt/run-hundo.sh
+    printf 'do_processing,False' > output_hundo/kvp.csv
+) > output_hundo/script-output.log 2>&1
